a94566b4e941ec1ebe0598fc37af51b43563dbcd
Rajat Mhetre <mhetrerajat@gmail.com>
2019-02-20 00:06:16 +0530

    add solution for tower of hanoi

---
+++ careermonk/tower_of_hanoi.py
@@ -0,0 +1,19 @@
+
+
+def tower_of_hanoi(n, from_rod, to_rod, aux_rod):
+    if n == 1:
+        print("{0} : {1} -> {2}".format(n, from_rod, to_rod))
+        return
+    
+    tower_of_hanoi(n-1, from_rod, aux_rod, to_rod)
+    print("{0} : {1} -> {2}".format(n, from_rod, to_rod))
+    tower_of_hanoi(n-1, aux_rod, to_rod, from_rod)
+
+
+def main():
+    n = 3
+    tower_of_hanoi(n, "A", "C", "B")
+
+
+if __name__ == "__main__":
+    main()
