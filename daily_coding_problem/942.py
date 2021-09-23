59c7830aca8384b53360ee82df7d73a77adb9ca2
Rajat Mhetre <mhetrerajat@gmail.com>
2021-09-24 00:15:43 +0530

    feat(dcp): add space efficient solution
    O(1) space complexity

--- daily_coding_problem/942.py
+++ daily_coding_problem/942.py
@@ -1,3 +1,19 @@
+def space_efficient(string: str) -> int:
+    balance = 0
+    added = 0
+    for item in string:
+        if item == "(":
+            balance += 1
+        else:
+            balance -= 1
+
+        if balance == -1:
+            balance += 1
+            added += 1
+
+    return added + balance
+
+
 def get_num_paranthesis_required(string: str) -> int:
     stack = []
     for item in string:
@@ -13,6 +29,13 @@ def get_num_paranthesis_required(string: str) -> int:
 
 
 if __name__ == "__main__":
-    test_cases = [("()())()", 1), (")(", 2), ("", 0), ("())", 1), ("(((", 3)]
+    test_cases = [
+        ("()())()", 1),
+        (")(", 2),
+        ("", 0),
+        ("())", 1),
+        ("(((", 3),
+        (")))", 3),
+    ]
     for (testcase, expectd_output) in test_cases:
-        assert get_num_paranthesis_required(testcase) == expectd_output
+        assert space_efficient(testcase) == expectd_output
