ef9a4315816dad3afd49b5c08228145b27882853
Rajat Mhetre <mhetrerajat@gmail.com>
2021-09-25 12:54:36 +0530

    feat(gfg): add memoize based solution

--- geeksforgeeks/tiling.py
+++ geeksforgeeks/tiling.py
@@ -1,15 +1,16 @@
 def get_possible_ways(n: int) -> int:
     """Tile can either be placed horizontally or vertically"""
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return get_possible_ways(n - 1) + get_possible_ways(n - 2)
+    memo = [0] * (n+1)
+    memo[0] = 0
+    memo[1] = 1
+
+    for i in range(2, n + 1):
+        memo[i] = memo[i - 1] + memo[i - 2]
+
+    return memo[-1]
 
 
 if __name__ == "__main__":
     testcases = [(4, 3), (3, 2), (2, 1), (1, 1)]
     for (testcase, expected_output) in testcases:
-        print(get_possible_ways(testcase))
         assert get_possible_ways(testcase) == expected_output
