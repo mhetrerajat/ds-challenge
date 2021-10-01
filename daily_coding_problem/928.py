0a5813a62240e39ba7afb67e9feb7e9123c61939
Rajat Mhetre <mhetrerajat@users.noreply.github.com>
2021-10-01 13:39:53 +0530

    Daily Coding Problem: Problem 928 (#18)

---
+++ daily_coding_problem/928.py
@@ -0,0 +1,24 @@
+def get_nth_perfect_num(N: int) -> int:
+    def is_perfect(num: int) -> bool:
+        sum = 0
+        while num > 0:
+            remainder = num % 10
+            num = num // 10
+            sum += remainder
+
+        return sum == 10
+
+    num = 19
+    nth_num = N
+    while True:
+        if is_perfect(num):
+            nth_num -= 1
+            if nth_num == 0:
+                return num
+        num += 9
+
+
+if __name__ == "__main__":
+    assert get_nth_perfect_num(1) == 19
+    assert get_nth_perfect_num(11) == 118
+    assert get_nth_perfect_num(10) == 109
