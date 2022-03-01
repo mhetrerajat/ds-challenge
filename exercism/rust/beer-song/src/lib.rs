915883a9222c6de53d7f36772e3af6fb8d9b911b
Rajat Mhetre <mhetrerajat@gmail.com>
2022-03-01 15:48:02 +0530

    refactor(exercism/rust): de deplicate code

--- exercism/rust/beer-song/src/lib.rs
+++ exercism/rust/beer-song/src/lib.rs
@@ -1,28 +1,19 @@
 pub fn verse(n: u32) -> String {
-    // unimplemented!("emit verse {}", n)
-
-    if n == 0 {
-        return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string();
-    } else if n == 1 {
-        return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string();
-    } else if n == 2 {
-        return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string();
+    match n {
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
+        _ => format!("{current} {fmt_bottles} of beer on the wall, {current} {fmt_bottles} of beer.\nTake one down and pass it around, {left} {left_fmt_bottles} of beer on the wall.\n", current=n, left=n-1, fmt_bottles=format_bottles(n), left_fmt_bottles=format_bottles(n-1))
     }
+}
 
-    format!("{current} bottles of beer on the wall, {current} bottles of beer.\nTake one down and pass it around, {left} bottles of beer on the wall.\n", current=n, left=n-1)
+fn format_bottles(n: u32) -> String {
+    match n {
+        1 => "bottle".to_string(),
+        _ => "bottles".to_string(),
+    }
 }
 
 pub fn sing(end: u32, start: u32) -> String {
-    // unimplemented!("sing verses {} to {}, inclusive", start, end)
-
-    let mut song = String::new();
-
-    for n in (start..=end).rev() {
-        song.push_str(&verse(n));
-        song.push_str("\n");
-    }
-
-    song.pop();
-
-    song
+    let song: Vec<String> = (start..=end).rev().map(verse).collect();
+    song.join("\n")
 }
