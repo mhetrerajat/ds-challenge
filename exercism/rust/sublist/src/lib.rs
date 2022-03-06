4293334a07899f095f2c04b478b72f8cba843971
Rajat Mhetre <mhetrerajat@gmail.com>
2022-03-06 12:56:43 +0530

    feat(exercism/rust): sublist

---
+++ exercism/rust/sublist/src/lib.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, PartialEq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    if _first_list == _second_list {
+        Comparison::Equal
+    } else if is_sublist(_first_list, _second_list) {
+        Comparison::Sublist
+    } else if is_sublist(_second_list, _first_list) {
+        Comparison::Superlist
+    } else {
+        Comparison::Unequal
+    }
+}
+
+fn is_sublist<T: PartialEq>(first: &[T], second: &[T]) -> bool {
+    first.is_empty()
+        || second
+            .windows(first.len())
+            .any(|subset| first.starts_with(subset))
+}
